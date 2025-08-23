2f794a95e9145b428526cc02f2eb6a5b77ef1a51
Karl Eknefelt <8060002+kalleankah@users.noreply.github.com>
2025-08-23 02:08:03 +0200

    Create TitleDetail sample

--- MustWatch/Repository/TitleDetailsRepository/TitleDetail.swift
+++ MustWatch/Repository/TitleDetailsRepository/TitleDetail.swift
@@ -78,3 +78,35 @@ extension TitleDetail.Rating {
         self.value = response.value
     }
 }
+
+extension TitleDetail {
+    static let sample = TitleDetail(
+        title: "The Shawshank Redemption",
+        year: "1994",
+        rated: "R",
+        released: "14 Oct 1994",
+        runtime: "142 min",
+        genre: "Drama",
+        director: "Frank Darabont",
+        writer: "Stephen King, Frank Darabont",
+        actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+        plot: "Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit. The film portrays the man's unique way of dealing with his new, torturous life; along the way he befriends a number of fellow prisoners, most notably a wise long-term inmate named Red.",
+        language: "English",
+        country: "United States",
+        awards: "Nominated for 7 Oscars. 21 wins & 42 nominations total",
+        poster: "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_SX300.jpg",
+        ratings: [
+            .init(source: "Internet Movie Database", value: "9.3/10"),
+            .init(source: "Rotten Tomatoes", value: "89%"),
+            .init(source: "Metacritic", value: "82/100")
+        ],
+        metascore: "82",
+        imdbRating: "9.3",
+        imdbVotes: "3,075,711",
+        imdbID: "tt0111161",
+        type: "movie",
+        dvd: "N/A",
+        boxOffice: "$28,767,189",
+        production: "N/A"
+    )
+}
