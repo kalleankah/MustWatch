e03749c121db4bbde15125589325cf7ac09b2738
Karl Eknefelt <8060002+kalleankah@users.noreply.github.com>
2025-08-24 13:09:03 +0200

    Improve title detail rows for large dynamic type

--- MustWatch/LandingTab/TitleDetail/TitleDetailView.swift
+++ MustWatch/LandingTab/TitleDetail/TitleDetailView.swift
@@ -132,18 +132,46 @@ struct TitleBasicSection: View {
 
             VStack(spacing: 12) {
                 ForEach(content, id: \.0) { heading, value in
-                    HStack(alignment: .top) {
-                        Text(heading)
-                        Spacer()
-                        Text(value)
-                            .font(.headline)
-                    }
+                    DynamicRow(heading: heading, value: value)
                 }
             }
             .padding()
             .background(.primary.opacity(0.1))
             .clipShape(RoundedRectangle(cornerRadius: 12))
         }
+    }
+}
+
+struct DynamicRow: View {
+    @Environment(\.dynamicTypeSize.isAccessibilitySize) private var isAccessibilitySize
+
+    var heading: String
+    var value: String
+
+    var body: some View {
+        if isAccessibilitySize {
+            verticalLayout
+        } else {
+            horizontalLayout
+        }
+    }
+
+    var horizontalLayout: some View {
+        HStack(alignment: .top) {
+            Text(heading)
+            Spacer()
+            Text(value)
+                .font(.headline)
+        }
+    }
+
+    var verticalLayout: some View {
+        VStack(alignment: .leading) {
+            Text(heading)
+            Text(value)
+                .font(.headline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
